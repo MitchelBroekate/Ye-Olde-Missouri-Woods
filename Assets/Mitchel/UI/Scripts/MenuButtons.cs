8f9e9e3ea50e7bb62c57189894dfeadd0c6c77a7
MitchelBroekate <mitchelb11@outlook.com>
2023-11-27 14:14:25 +0100

    Art for UI mainmenu

--- Assets/Mitchel/UI/Scripts/MenuButtons.cs
+++ Assets/Mitchel/UI/Scripts/MenuButtons.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuButtons : MonoBehaviour
 {
+    #region Variables
     public GameObject mainMenu;
     public GameObject settingsMenu;
+    public GameObject videoSettings;
+    public GameObject audioSettings;
     public GameObject quitOptions;
-    public void PlayButton()
+    #endregion
+
+    #region Main Menu Buttons
+    public void StartButton()
     {
-        
+        SceneManager.LoadScene("Main Game");
     }
 
     public void SettingsButton()
@@ -23,16 +30,12 @@ public class MenuButtons : MonoBehaviour
         mainMenu.SetActive(false);
         quitOptions.SetActive(true);
     }
+    #endregion
 
+    #region Quit Options Buttons
     public void QuitGame()
     {
         Application.Quit();
-    }
-
-    public void SettingsBackToMenu()
-    {
-        settingsMenu.SetActive(false);
-        mainMenu.SetActive(true);
     }
 
     public void QuitBackToMenu()
@@ -40,5 +43,33 @@ public class MenuButtons : MonoBehaviour
         quitOptions.SetActive(false);
         mainMenu.SetActive(true);
     }
+    #endregion
 
+    #region Settings Menu Buttons
+    public void SettingsBackToMenu()
+    {
+        settingsMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+
+    public void VideoSettings()
+    {
+        if(audioSettings.activeInHierarchy == true)
+        {
+            audioSettings.SetActive(false);
+        }
+
+        videoSettings.SetActive(true);
+    }
+
+    public void AudioSettings()
+    {
+        if(videoSettings.activeInHierarchy == true)
+        {
+            videoSettings.SetActive(false);
+        }
+
+        audioSettings.SetActive(true);
+    }
+    #endregion
 }
