3c063d30885a63ca43fadcabcee129b4a88822ab
Dennis Bakker <dennis--bakker@hotmail.com>
2024-01-22 11:07:42 +0100

    Apple Prefab

--- Assets/Dennis/Scripts/Health Regen/AppleSfx.cs
+++ Assets/Dennis/Scripts/Health Regen/AppleSfx.cs
@@ -2,14 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AppleSfx : MonoBehaviour
+public class AppleSFX : MonoBehaviour
 {
     public AudioSource clip;
+    public Rigidbody rb;
+
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag == "Ground")
         {
             clip.Play();
+            rb.isKinematic = true;
         }
     }
 }
