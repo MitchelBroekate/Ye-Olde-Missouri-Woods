45af9379707ecd6c2e744ad03b85169bf76a06f3
MitchelBroekate <mitchelb11@outlook.com>
2024-01-25 13:06:43 +0100

    objectives placed and linked

---
+++ Assets/Mitchel/Objective/ObjectiveSystem.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectiveSystem : MonoBehaviour
+{
+    [Header("Enemy Area")]
+    public GameObject area1;
+    public GameObject area2;
+    public GameObject area3;
+    public GameObject area4;
+    public GameObject area5;
+    public GameObject area6;
+
+    [Header("Audio")]
+    public AudioSource audioSource;
+    public AudioClip intro;
+    public AudioClip tutorial;
+    public AudioClip attack1;
+    public AudioClip attack2;
+    public AudioClip attack3;
+    public AudioClip attack4;
+    public AudioClip attack5;
+    public AudioClip attack6;
+    public AudioClip outro;
+
+
+    [Header("Checkpoint Location")]
+    public GameObject v1;
+    public GameObject v2;
+    public GameObject v3;
+    public GameObject v4;
+    public GameObject v5;
+
+    [Header("Canvas")]
+    [SerializeField]
+    GameObject canvas;
+
+    private void Start()
+    {
+        audioSource.clip = intro;
+        canvas.transform.position = v1.transform.position;
+        if (!audioSource.isPlaying)
+        {
+            audioSource.clip = tutorial;
+        }
+
+        if (!audioSource.isPlaying)
+        {
+            audioSource.clip = attack1;
+        }
+    }
+
+    private void Update()
+    {
+        if(area1.transform.childCount < 1)
+        {
+            audioSource.clip = attack2;
+            canvas.transform.position = v2.transform.position;
+        }
+
+        if (area2.transform.childCount < 1)
+        {
+            audioSource.clip = attack3;
+            canvas.transform.position = v3.transform.position;
+        }
+
+        if (area3.transform.childCount < 1)
+        {
+            audioSource.clip = attack4;
+            canvas.transform.position = v4.transform.position;
+        }
+
+        if (area4.transform.childCount < 1)
+        {
+            audioSource.clip = attack5;
+            canvas.transform.position = v5.transform.position;
+        }
+
+        if (area5.transform.childCount < 1)
+        {
+            audioSource.clip = attack6;
+            canvas.transform.position = v1.transform.position;
+        }
+
+        if (area6.transform.childCount < 1)
+        {
+            audioSource.clip = outro;
+            canvas.SetActive(false);
+        }
+
+    }
+}
